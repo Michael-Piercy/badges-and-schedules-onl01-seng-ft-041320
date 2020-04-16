7722be5a9ddaa9b3a9b5dba681a732db98e0d263
Michael Piercy <michael.piercy@zoho.com>
2020-04-16 20:42:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(badges)
+def batch_badge_creator(badge)
   name.each do |badge|
   "Hello, my name is #{badge}."
   end
